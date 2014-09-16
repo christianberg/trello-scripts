a07136e1c84de35f839df8a63669d1f1fa7c7bc1
Christian Berg <berg.christian@gmail.com>
2014-09-16 17:28:56 +0200

    Initialize new board by removing all lists

--- lib/sync_dir_to_board.rb
+++ lib/sync_dir_to_board.rb
@@ -1,7 +1,6 @@
 require 'trello'
 
 class SyncDirToBoard
-
   def initialize(settings = {})
     Trello.configure do |config|
       config.developer_public_key = settings['auth']['key']
@@ -10,20 +9,26 @@ class SyncDirToBoard
 
     @me = Trello::Member.find(settings['user'])
     @board = find_or_create_board(settings['board'])
+    @default_list = settings['default_list']
+    initialize_board(@board)
   end
 
   def find_or_create_board(name)
     matching_boards = @me.boards.select { |b| b.name == name }
     if matching_boards.empty?
-      puts "#{name} not found"
       Trello::Board.create(name: name)
     else
-      puts 'board found: ' + matching_boards.first.id
       matching_boards.first
     end
   end
 
-  def run!
+  # Only if the board contains no cards at all, remove all lists. This
+  # is used to remove the default lists from a newly created board.
+  def initialize_board(board)
+    return unless board.cards.empty?
+    board.lists.each(&:close!)
   end
 
+  def run!
+  end
 end
